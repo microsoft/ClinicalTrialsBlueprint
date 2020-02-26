956c296328cdc60858a1c29b0b729da4a57eb1b1
Arie <arie@schwartzman.info>
2020-02-26 19:18:16 +0200

    wip

--- scripts/azuredeploy-healthcarebot.ps1
+++ scripts/azuredeploy-healthcarebot.ps1
@@ -78,7 +78,12 @@ Try {
         # Here you need to replace the place holders with real data
 
         $restoreJSON = $restoreJSON.Replace('{ctm-api-key}', $matchingParameters.proxyApiKey.Value)
-        $restoreJSON = $restoreJSON.Replace('{qe-baseurl}', $qeBaseUrl)
+        $restoreJSON = $restoreJSON.Replace('{qe-baseurl}', $matchingParameters.queryEngineEndpoint.Value)
+        $restoreJSON = $restoreJSON.Replace('{dcs-baseurl}', $matchingParameters.dynamicCriteriaSelectionEndpoint.Value)
+        $restoreJSON = $restoreJSON.Replace('{disq-baseurl}', $matchingParameters.dynamicCriteriaSelectionEndpoint.Value)
+        $restoreJSON = $restoreJSON.Replace('{luisApplicationId}', $luisApplicationId)
+        $restoreJSON = $restoreJSON.Replace('{luisPredictionKey}', $output.Outputs["luisPredictionKey"].Value)
+        $restoreJSON = $restoreJSON.Replace('{luisLocation}', $output.Parameters["luisLocation"].Value)
 
         $saasTenant = Restore-HbsTenant -location $botLocation -tenant $saasTenant `
                                         -data $restoreJSON -saasSubscriptionId $saasSubscriptionId
