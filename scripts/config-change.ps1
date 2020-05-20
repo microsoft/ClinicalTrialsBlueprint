dac4594bcbbbe14fbe246b632387e07e90f117eb
Ronny Roktel <ronny.roktel@microsoft.com>
2020-05-20 13:17:26 +0300

    Dont stop the secondary aci, to run update with updated config

--- scripts/config-change.ps1
+++ scripts/config-change.ps1
@@ -12,20 +12,23 @@ function ChangeConfig(
   $subscriptionId = (Get-AzContext).Subscription.Id
 	$primaryAci = "$serviceName-ctm-struct-aci"
 	$secondaryAci = "$serviceName-ctm-struct-aci-s"
+	$gatewayWebApp = "$serviceName-gateway-webapp"
 
 	$stopped = IsAciStopped -resourceGroupName $resourceGroupName -aciName $primaryAci
-	if (-Not $stopped) {
+	if(-Not $stopped) {
 		Write-Host "Cannot change config when a container group is running $primaryAci" -ForegroundColor Red 
 		return
 	} 
 	Write-Host "$primaryAci is stopped, continuing..." -ForegroundColor Green 
 	
 	$stopped = IsAciStopped -resourceGroupName $resourceGroupName -aciName $primaryAci
-	if (-Not $stopped) {
+	if(-Not $stopped) {
 		Write-Host "Cannot change config when a container group is running $secondaryAci" -ForegroundColor Red 
 		return
-	} 
+	}
 	Write-Host "$secondaryAci is stopped, continuing..." -ForegroundColor Green
+
+	$isPrimary = IsCurrentSlotPrimary -gatewayWebApp $gatewayWebApp
 
 	Write-Host "deploying primary resources"
 
@@ -33,15 +36,22 @@ function ChangeConfig(
 	$matchingOutput = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -serviceName $serviceName `
 		-TemplateFile ".\arm-templates\azuredeploy-ctm.json" -TemplateParameterFile ".\arm-templates\azuredeploy-ctm.parameters.json" 
 
-	StopAci -resourceGroupName $resourceGroupName -aciName $primaryAci
+	# stopping the primary slot's aci
+	if ($isPrimary) {
+		StopAci -resourceGroupName $resourceGroupName -aciName $primaryAci
+	}
 
 	Write-Host "deploying secondary resources"
 
+	# deploy the secondary resources
 	$matchingOutput = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -serviceName $serviceName `
 		-TemplateFile ".\arm-templates\azuredeploy-ctm.json" -TemplateParameterFile ".\arm-templates\azuredeploy-ctm.parameters.json" `
 		-isSecondary $true 
 
-	StopAci -resourceGroupName $resourceGroupName -aciName $secondaryAci
+	# stopping the primary slot's aci
+	if (-Not $isPrimary) {
+		StopAci -resourceGroupName $resourceGroupName -aciName $secondaryAci
+	}
 
 	Write-Host "Config change finished successfully." -ForegroundColor Green	
 }
@@ -61,6 +71,17 @@ function IsAciStopped (
 																					-Headers $headers
 	$content = $aciDetails.Content | ConvertFrom-Json
 	return $content.properties.instanceView.state -eq "Stopped"
+}
+
+function IsCurrentSlotPrimary(
+	[Parameter(Mandatory=$true)]
+	$gatewayWebApp
+)
+{
+	$gatewayDetails = Invoke-WebRequest -Uri "https://$gatewayWebApp.azurewebsites.net/slot" `
+																			-Method "get"
+	$content = $gatewayDetails.Content | ConvertFrom-Json
+	return $content.slot -eq "primary"
 }
 
 function StopAci(
