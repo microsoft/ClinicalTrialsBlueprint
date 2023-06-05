93518e475ae6ffeffd70c0771191b0c8e5624b5b
yochai lehman <yolehman@microsoft.com>
2023-06-05 10:28:52 +0300

    update script

--- scripts/RestoreLanguageUnderstanding.ps1
+++ scripts/RestoreLanguageUnderstanding.ps1
@@ -1,5 +1,5 @@
 param (
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$fileLocation
 )
 
@@ -7,39 +7,80 @@ $cuiKey = $env:CLU_KEY
 $cuiEndpoint = $env:CLU_ENDPOINT
 
 
-# Import the file into a new Cognitive Language Understanding project named "metadata_clinical_trials"
-$projectName = "metadata_clinical_trials"
+# Import the file into a new Cognitive Language Understanding project named "clinical_trial_metadata"
+$projectName = "clinical_trial_metadata"
+$modelLabel = 'clinical_trial_metadata'
 $apiVersion = '?api-version=2022-10-01-preview'
 
 # Set the headers for the request
 $headers = @{
     "Ocp-Apim-Subscription-Key" = $cuiKey
-    "Content-Type" = "application/json"
+    "Content-Type"              = "application/json"
 }
+
+function WaitForJob {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$statusEndpoint
+    )
+    Write-Warning "Waiting for job: $statusEndpoint"
+    do {
+        Start-Sleep -Seconds 5
+        $statusResponse = Invoke-WebRequest -Method Get -Uri $statusEndpoint -Headers $headers
+        $status = ($statusResponse.Content | ConvertFrom-Json).status
+        Write-Host "Status: $status"
+    } while ($status -match "notStarted|running")
+
+    return $status
+}
+
+
+
+
 
 # Fetch json file from web address
 $jsonFile = Invoke-WebRequest -Uri  "$fileLocation/clu/metadata_clinical_trials.json" 
 
 # Send the request to import the project
 $bodyJson = $jsonFile.Content
-$importUri = $cuiEndpoint + "language/authoring/analyze-conversations/projects/$projectName/:import"+ $apiVersion
+$importUri = $cuiEndpoint + "language/authoring/analyze-conversations/projects/$projectName/:import" + $apiVersion
 Write-Warning "Importing file: $importUri"
 $importResponse = Invoke-WebRequest -Method Post `
-                    -Uri $importUri `
-                    -Headers $headers -Body $bodyJson
+    -Uri $importUri `
+    -Headers $headers -Body $bodyJson
+
+WaitForJob $importResponse.Headers['operation-location']
 
 Write-Warning   "Import status: $($importResponse.Content)"
 
 # Train the project
-$trainUri = $cuiEndpoint + "language/authoring/analyze-conversations/projects/$projectName/train" + $apiVersion
-$trainResponse = Invoke-WebRequest -Method Post -Uri $trainUri -Headers $headers
+$trainUri = $cuiEndpoint + "language/authoring/analyze-conversations/projects/$projectName/:train" + $apiVersion
+$bodyJson = @{
+    'modelLabel'            = $modelLabel
+    'trainingMode'          = 'standard'
+    'trainingConfigVersion' = 'latest'
+    'evaluationOptions'     = @{
+        'kind'                    = 'percentage'
+        'testingSplitPercentage'  = 20
+        'trainingSplitPercentage' = 80
+    }
+}
+
+$trainResponse = Invoke-WebRequest -Method Post -Uri $trainUri -Headers $headers -Body $bodyJson
 
 Write-Warning "Train status: $($trainResponse.Content)"
+WaitForJob $importResponse.Headers['operation-location']
 
 # Deploy the project
-$deployUri = $cuiEndpoint + "language/authoring/analyze-conversations/projects/$projectName/deploy" + $apiVersion
-$deployResponse = Invoke-WebRequest -Method Post `
-                  -Uri $deployUri `
-                  -Headers $headers
+$deployUri = $cuiEndpoint + "language/authoring/analyze-conversations/projects/$projectName/deployments/deploy" + $apiVersion
+$bodyJson = @{
+    'trainedModelLabel' = $modelLabel
+}
+
+$deployResponse = Invoke-WebRequest -Method Put `
+    -Uri $deployUri `
+    -Headers $headers `
+    -Body $bodyJson
 
 Write-Warning "Deploy status: $($deployResponse.Content)"
+WaitForJob $importResponse.Headers['operation-location']
