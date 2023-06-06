0c43f72def638b62cc4b41e0be7ca481b2512055
yochai lehman <yolehman@microsoft.com>
2023-06-06 12:56:21 +0300

    update

--- scripts/RestoreLanguageUnderstanding.ps1
+++ scripts/RestoreLanguageUnderstanding.ps1
@@ -12,18 +12,18 @@ $projectName = "clinical_trial_metadata"
 $modelLabel = 'clinical_trial_metadata'
 $apiVersion = '?api-version=2022-10-01-preview'
 
-# Set the headers for the request
+$contentType = 'application/json; charset=UTF-8'
 $headers = @{
     "Ocp-Apim-Subscription-Key" = $cuiKey
-    "Content-Type"              = 'application/json; charset=UTF-8'
-    "Accept-Charset"= 'UTF-8'
-    "Accept" = "*/*"
+    "Content-Type"              = $contentType
+    "Accept-Charset"            = 'UTF-8'
+    "Accept"                    = "*/*"
 }
 
 function WaitForJob {
     param (
         [Parameter(Mandatory = $true)]
-        [string]$statusEndpoint
+        [string]$StatusEndpoint
     )
     Write-Warning "Waiting for job: $statusEndpoint"
     do {
@@ -51,39 +51,46 @@ function Call-Http {
         
         Write-Warning "Calling $Method $Uri with body length $($Body.Length)"
         if ($Body) {
-            $response = Invoke-WebRequest -Method $Method -Uri $Uri -Headers $headers -Body $Body -ErrorAction Stop -ContentType "application/json"
-        } else {
-            $response = Invoke-WebRequest -Method $Method -Uri $Uri -Headers $headers -ErrorAction Stop
+            $response = Invoke-WebRequest -Method $Method -Uri $Uri -Headers $headers -Body $Body -ErrorAction Stop -ContentType $contentType
+        }
+        else {
+            $response = Invoke-WebRequest -Method $Method -Uri $Uri -Headers $headers -ErrorAction Stop -ContentType $contentType
         }
         return $response
-    } catch {
+    }
+    catch {
         Write-Host "HTTP request failed with the following response:"
         Write-Host $_.Exception
-        Write-Host $_.Exception.Response.StatusCode.value__ 
-        Write-Host $_.Exception.Response.StatusDescription
-        $result = $_.Exception.Response.GetResponseStream()
-        $reader = New-Object System.IO.StreamReader($result)
-        $reader.BaseStream.Position = 0
-        $reader.DiscardBufferedData()
-        $responseBody = $reader.ReadToEnd();
-        Write-Host $responseBody
-        throw $_.Exception.Response.Content
+        if (Write-Host $_.Exception.Response -ne $null) {
+            Write-Host $_.Exception.Response.StatusCode.value__ 
+            Write-Host $_.Exception.Response.StatusDescription
+            $result = $_.Exception.Response.GetResponseStream()
+            $reader = New-Object System.IO.StreamReader($result)
+            $reader.BaseStream.Position = 0
+            $reader.DiscardBufferedData()
+            $responseBody = $reader.ReadToEnd();
+            Write-Host $responseBody
+        }
+        throw
     }
 }
 
 
 # Fetch json file from web address
-$jsonFile = Call-Http  -Method Get -Uri  "$fileLocation/clu/clinical_trial_metadata.json"
+$jsonFile = Invoke-WebRequest  -Method Get -Uri  "$fileLocation/clu/clinical_trial_metadata.json" -ContentType $contentType
 
 # Send the request to import the project
 $bodyJson = $jsonFile.Content
+
+$bodyJson | Out-File -FilePath ".\bodyJson.json" -Encoding utf8
 
 $importUri = $cuiEndpoint + "language/authoring/analyze-conversations/projects/$projectName/:import" + $apiVersion
 $importResponse = Call-Http -Method Post `
     -Uri $importUri `
     -Body $bodyJson
 
-WaitForJob $importResponse.Headers['operation-location']
+$importLocation = $importResponse.Headers['operation-location'][0]
+WaitForJob -StatusEndpoint $importLocation
 
 Write-Warning   "Import status: $($importResponse.Content)"
 
@@ -101,21 +108,23 @@ $bodyJson = @{
 } | ConvertTo-Json -Depth 4
 
 $trainResponse = Call-Http -Method Post `
- -Uri $trainUri `
- -Body $bodyJson
+    -Uri $trainUri `
+    -Body $bodyJson
 
 Write-Warning "Train status: $($trainResponse.Content)"
-WaitForJob $importResponse.Headers['operation-location']
+$trainLocation = $trainResponse.Headers['operation-location'][0]
+WaitForJob -StatusEndpoint $trainLocation
 
 # Deploy the project
 $deployUri = $cuiEndpoint + "language/authoring/analyze-conversations/projects/$projectName/deployments/deploy" + $apiVersion
 $bodyJson = @{
     'trainedModelLabel' = $modelLabel
-}| ConvertTo-Json -Depth 4
+} | ConvertTo-Json -Depth 4
 
 $deployResponse = Call-Http -Method Put `
     -Uri $deployUri `
     -Body $bodyJson
 
 Write-Warning "Deploy status: $($deployResponse.Content)"
-WaitForJob $importResponse.Headers['operation-location']
+$deployLocation = $deployResponse.Headers['operation-location'][0]
+WaitForJob -StatusEndpoint $deployLocation
